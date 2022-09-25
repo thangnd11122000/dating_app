c91ad614579aad8e018da7f3bbcaf3065a66f630
thang <thangnd111220@gmail.com>
2022-09-25 22:11:24 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import Header from "./components/Header";
+import DatingCards from "./components/DatingCards";
+import SwipeButtons from "./components/SwipeButtons";
 function App() {
   return (
     <div className="app">
-            <h1>Dating App MERN </h1>
-          
+      <Header />
+      <DatingCards />
+      <SwipeButtons />
     </div>
   );
 }
